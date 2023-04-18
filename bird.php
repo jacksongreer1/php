b57b5d680e2b8bca3e62edae174faa9370eb9bd6
Jackson Greer <97543055+jacksongreer1@users.noreply.github.com>
2023-04-18 07:55:13 -0500

    bh

---
+++ bird.php
@@ -0,0 +1,191 @@
+<!doctype html> 
+
+<html lang="en"> 
+
+  <head> 
+
+    <meta charset="utf-8"> 
+
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+    <title>Biggest Birds</title> 
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> 
+
+  <link rel="stylesheet" href="style.css"> 
+
+  </head> 
+
+  <body> 
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary"> 
+
+        <div class="container-fluid"> 
+
+          <a class="navbar-brand" href="bird.html"></a> 
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
+
+            <span class="navbar-toggler-icon"></span> 
+
+          </button> 
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0"> 
+
+              <li class="nav-item"> 
+
+                <a class="nav-link active" aria-current="page" href="index.html">Home</a> 
+
+              </li> 
+
+              <li class="nav-item"> 
+
+                <a class="nav-link" href="about.html">About</a> 
+
+              </li> 
+
+              <li class="nav-item"> 
+
+                <a class="nav-link" href="bird.html">Birds</a> 
+
+              </li> 
+
+              <li class="nav-item"> 
+
+                <a class="nav-link" href="supplies.html">Supplies</a> 
+
+              </li> 
+
+              <li class="nav-item"> 
+
+                <a class="nav-link" href="food.html">Food</a> 
+
+              </li> 
+
+              
+
+              
+
+            </ul> 
+
+            <form class="d-flex" role="search"> 
+
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> 
+
+              <button class="btn btn-outline-success" type="submit">Checkout</button> 
+
+            </form> 
+
+          </div> 
+
+        </div> 
+
+      </nav> 
+
+       
+
+      
+
+      <div class="container p-5"> 
+
+        <div class="row"> 
+
+          <div class="col-md-4"> 
+
+            <div class="card" style="width: 18rem;"> 
+
+              <img src="img/worms.jpg" class="card-img-top" alt="..."> 
+
+              <div class="card-body"> 
+
+                <h5 class="card-title">Worms</h5> 
+
+                <p class="card-text"> $45 Amazing protein for your bird!!!! 
+
+                  .</p> 
+
+                <a href="#" class="btn btn-primary">Add To Cart</a> 
+
+              </div> 
+
+            </div>  
+
+             
+
+            
+
+          </div> 
+
+ 
+ 
+ 
+
+          <div class="col-md-4"> 
+
+            <div class="card" style="width: 18rem;"> 
+
+              <img src="img/sunflower.jpg" class="card-img-top" alt="..."> 
+
+              <div class="card-body"> 
+
+                <h5 class="card-title">Sunflower Seeds</h5> 
+
+                <p class="card-text">$40 A tasty snack your bid will enjoy!! </p> 
+
+                <a href="#" class="btn btn-primary">Add To Cart</a> 
+
+              </div> 
+
+            </div>  
+
+          </div> 
+
+            
+
+           
+
+ 
+ 
+
+          <div class="col-md-4"> 
+
+            <div class="card" style="width: 18rem;"> 
+
+              <img src="img/bird food.jpg" class="card-img-top" alt="..."> 
+
+              <div class="card-body"> 
+
+                <h5 class="card-title">Premium Bird Food</h5> 
+
+                <p class="card-text">Price $50 Voted the best bird food by the National Health Department!!</p> 
+
+                <a href="#" class="btn btn-primary">Add To Cart</a> 
+
+              </div> 
+
+            </div>  
+
+ 
+ 
+
+            
+
+            </div> 
+
+       
+
+    </div> 
+
+     
+
+  </div> 
+
+    
+
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,96L26.7,96C53.3,96,107,96,160,106.7C213.3,117,267,139,320,160C373.3,181,427,203,480,181.3C533.3,160,587,96,640,96C693.3,96,747,160,800,181.3C853.3,203,907,181,960,170.7C1013.3,160,1067,160,1120,144C1173.3,128,1227,96,1280,101.3C1333.3,107,1387,149,1413,170.7L1440,192L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg> 
+
+  </body> 
+
+</html> 
